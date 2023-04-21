3af19dbbc3d14e90891f2a81ee99ce2dc5b39db6
Bruno <brpadilha.dev@gmail.com>
2023-04-21 19:44:58 -0300

    ajusta url das imagens para prod

--- src/components/Product/ProductList.tsx
+++ src/components/Product/ProductList.tsx
@@ -21,7 +21,7 @@ export function ProductList({ name, description, promotionPrice, price, images }
           images.map(image => (
             <li key={image.id}>
               <Image
-                src="https://images-prod.mmartan.com.br/1536x1536/png/products/photos/semi-environment/_mg_4834_kqn28gblo21mr-gargeurbo16tr-vasunbano22dv-mqmif-1658343252695.png"
+                src={`https://raw.githubusercontent.com/brpadilha/backend-ammo-varejo-challenge/main/src${image.url}`}
               />
             </li>
           ))
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}`,
+  baseURL: `${import.meta.env.VITE_API_URL}` || 'http://localhost:3000',
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    "Access-Control-Allow-Methods": "GET",
     "Access-Control-Allow-Headers": "Content-Type, Authorization, Content-Length, X-Requested-With",
-    "Authorization":"https://frontend-ammo-varejo-challenge.vercel.app"
 }
 });
 
