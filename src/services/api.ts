1ff1336010eec7218f5169cd0e0644708fbf413e
Bruno <brpadilha.dev@gmail.com>
2023-04-21 21:26:39 -0300

    adiciona origin

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,12 +4,10 @@ const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}` || 'http://localhost:3000',
   headers: {
     "Content-Type": "application/json;charset=utf-8",
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin": "https://frontend-ammo-varejo-challenge.vercel.app",
     "Access-Control-Allow-Methods": "GET",
     "Access-Control-Allow-Headers": "X-CSRF-TOKEN, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
     "Access-Control-Allow-Credentials": "true",
-    "referer":'https://frontend-ammo-varejo-challenge.vercel.app',
-    "METHOD": "GET",
 }
 });
 
