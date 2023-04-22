60e9299e8f7240b42a7fe861a43183e3fff7d078
Bruno <brpadilha.dev@gmail.com>
2023-04-21 21:14:17 -0300

    tenta adicionar mais configuracoes para problema de cors'

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,12 +3,13 @@ import axios from "axios";
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}` || 'http://localhost:3000',
   headers: {
-    "Content-Type": "application/json",
+    "Content-Type": "application/json;charset=utf-8",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET",
     "Access-Control-Allow-Headers": "X-CSRF-TOKEN, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
     "Access-Control-Allow-Credentials": "true",
-
+    "referer":'https://frontend-ammo-varejo-challenge.vercel.app',
+    "METHOD": "GET",
 }
 });
 
