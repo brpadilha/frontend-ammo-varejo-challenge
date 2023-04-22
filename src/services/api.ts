46b78e6296fb0bf3f5d201b32f787bb1a052c678
Bruno <brpadilha.dev@gmail.com>
2023-04-21 21:38:06 -0300

    remove headers da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,14 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}` || 'http://localhost:3000',
-  headers: {
-    "Content-Type": "application/json;charset=utf-8",
-    "Access-Control-Allow-Origin": "https://frontend-ammo-varejo-challenge.vercel.app",
-    "Access-Control-Allow-Methods": "GET",
-    "Access-Control-Allow-Headers": "X-CSRF-TOKEN, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-    "Access-Control-Allow-Credentials": "true",
-}
+  baseURL: `${import.meta.env.VITE_API_URL}` || 'http://localhost:3000'
 });
 
 export default api;
